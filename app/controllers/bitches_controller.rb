5553ffb39107d422d633db081d047b923f48d68d
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2020-01-08 19:29:55 +0900

    Update bitches_controller.rb

--- app/controllers/bitches_controller.rb
+++ app/controllers/bitches_controller.rb
@@ -1,2 +1,21 @@
 class BitchesController < ApplicationController
+
+  def index
+    @bitches = Bitch.all
+  end
+
+  def create
+
+  end
+
+  private
+
+  def set_bitch
+    @bitch = Bitch.find(params[:id])
+  end
+
+  def bitch_params
+    params.require(:bitch).permit(:comment)
+  end
+
 end
