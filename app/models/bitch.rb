857b6d5605888d621c0a7825b27d1972212df161
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2020-01-12 16:02:47 +0900

    Update bitch.rb

--- app/models/bitch.rb
+++ app/models/bitch.rb
@@ -1,5 +1,8 @@
 class Bitch < ApplicationRecord
   validates :comment, presence: true, length: { maximum: 200 }
   belongs_to :user
-  has_many :empathies
+  has_many :empathies, dependent: :destroy
+  def empathies_user(user_id)
+    empathies.find_by(user_id: user_id)
+  end
 end
