8588adf2dc1b45fba50d8c7d3450dd1d21f93d68
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2020-01-12 16:02:35 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "bitches#index"
 
-  resources :bitches
-  post '/bitches', to: 'bitches#empathy'
+  resources :bitches do
+    resources :empathies, only: [:create, :destroy]
+  end
+  # post 'empathies/:bitch_id/create', to: 'bitches#empathy_create', constraints: {bitch_id: /\d+/}, as: :empathy_create
+  # post 'empathies/:bitch_id/delete', to: 'bitches#empathy_delete', constraints: {bitch_id: /\d+/}, as: :empathy_delete
 end
