12d1d7039635d9551433727191de8a96e936dbcc
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2020-01-12 16:02:49 +0900

    Update empathies_controller.rb

--- app/controllers/empathies_controller.rb
+++ app/controllers/empathies_controller.rb
@@ -1,2 +1,15 @@
 class EmpathiesController < ApplicationController
+
+  def create
+    @empathy = Empaty.create(user_id: current_user.id, bitch_id: params[:bitch_id])
+    @empathies = Empathy.where(bitch_id: params[:bitch_id])
+    @bitches = Bitch.all
+  end
+
+  def delete
+    @empathy = Empathy.find_by(user_id: @current_user.id, bitch_id: params[:bitch_id])
+    @empathy.destroy
+    @empathies = Empathy.where(bitch_id: params[:bitch_id])
+    @bitches = Bitch.all
+  end
 end
