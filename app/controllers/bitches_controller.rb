3acfb5be28bd4c8ac4c1496af422b111af659809
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2020-01-12 12:20:28 +0900

    Update bitches_controller.rb

--- app/controllers/bitches_controller.rb
+++ app/controllers/bitches_controller.rb
@@ -3,6 +3,7 @@ class BitchesController < ApplicationController
 
   def index
     @bitches = Bitch.all
+    @user = User.all
   end
 
   def new
@@ -10,9 +11,16 @@ class BitchesController < ApplicationController
   end
 
   def create
-
+    Bitch.create(bitch_params)
   end
 
+  def destroy
+    if @bitch.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
   private
 
   def set_bitch
@@ -20,7 +28,7 @@ class BitchesController < ApplicationController
   end
 
   def bitch_params
-    params.require(:bitch).permit(:comment)
+    params.require(:bitch).permit(:comment).marge(user_id: current_user.id)
   end
 
 
