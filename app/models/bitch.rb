5dd371027678dcb4a352826429d66195229beddf
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2020-01-16 11:37:11 +0900

    description create

--- app/models/bitch.rb
+++ app/models/bitch.rb
@@ -1,5 +1,5 @@
 class Bitch < ApplicationRecord
-  validates :comment, presence: true, length: { maximum: 200 }
+  validates :comment, presence: true, length: { maximum: 800 }
   belongs_to :user
   has_many :empathies, dependent: :destroy
   has_many :empathied_users, through: :empathies, source: :user
